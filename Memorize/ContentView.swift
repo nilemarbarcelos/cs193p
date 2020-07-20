cdf2bff16f32cf978bf9f904182ebc45398cf3f8
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-07-20 20:12:15 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Nilemar de Barcelos on 20/07/2020.
+//  Copyright © 2020 Nilemar Barcelos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
